5713516bb1e8777acd8a883030d0097260774d45
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-02-15 23:22:03 +0530

    http methods and request response

--- main.py
+++ main.py
@@ -4,4 +4,34 @@ app = FastAPI()
 
 @app.get('/')
 def home():
-    return {"msg":"hello world"}
+    return items
+
+items = [{"name":"ayuh","age":21},{"name":"ayush","age":21,"city":"bhopal"}]
+
+@app.post("/create")
+def create( name :str,age:int,city:str):
+    d = {}
+    d["name"] = name
+    d["age"] = age
+    d["city"] = city
+    items.append(d)
+    print(items)
+    return items
+
+@app.put("/update/{name}")
+def update(name):
+    for i in items:
+        if i["name"] == name:
+            i["name"] = "piyush"
+            i["age"] = 32
+            i["city"] = "indore"
+        print(i)
+    return i
+
+@app.delete("/del/{name}")
+def delete(name):
+    for i in items:
+        if i["name"] == name:
+            i["name"] = ""
+        print(i)
+    return i
